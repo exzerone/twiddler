561a7689c199d3a28d16f327d4de0dcd909f9a9b
Chris Huang <chrishuang128@gmail.com>
2019-03-25 00:09:26 -0700

    Add the code to generate timeline for each user

--- app.js
+++ app.js
@@ -12,14 +12,15 @@ $(document).ready(function(){
     index -= 1;
   }
 
+  // $(".users").on('click', function(){
+  //   filter()
+  // });
 
 
 });
 
-
-
-
   function newTweet(){
+   
     var tweet = streams.home[Math.floor(Math.random() * streams.home.length)];
     var $tweet = $('<div class = newTweet></div>');
     generateRandomTweet()
@@ -29,28 +30,56 @@ $(document).ready(function(){
   }
 
   function mracus(){
-    var randomize = streams.users.mracus[Math.floor(Math.random() * streams.users.mracus.length)]; 
-    var $tweet = $('<div class= userTweet></div>');
-    $tweet.text('@' + randomize.user + ': ' + randomize.message + ' posted at ' + randomize.created_at );
-    
-    $tweet.appendTo(".newsfeed");
-   
+    generateRandomTweet()
+    var $main = $('.main');
+    $main.html('');
+    var index = streams.users.mracus.length-1;
+    while ( index >= 0){
+      var randomize = streams.users.mracus[Math.floor(Math.random() * streams.users.mracus.length)]; 
+      var $tweet = $('<div class= userTweet></div>');
+      $tweet.text('@' + randomize.user + ': ' + randomize.message + ' posted at ' + randomize.created_at );
+      $tweet.appendTo(".newsfeed");
+      index -= 1
+    }
   }
   function sharksforcheap(){
-    var randomize = streams.users.sharksforcheap[Math.floor(Math.random() * streams.users.sharksforcheap.length)]; 
-    var $tweet = $('<div class= userTweet></div>');
-    $tweet.text('@' + randomize.user + ': ' + randomize.message + ' posted at ' + randomize.created_at );
-    $tweet.appendTo(".newsfeed");
+    generateRandomTweet()
+    var $main = $('.main');
+    $main.html('');
+    var index = streams.users.sharksforcheap.length-1;
+    while ( index >= 0){
+      var randomize = streams.users.sharksforcheap[Math.floor(Math.random() * streams.users.sharksforcheap.length)]; 
+      var $tweet = $('<div class= userTweet></div>');
+      $tweet.text('@' + randomize.user + ': ' + randomize.message + ' posted at ' + randomize.created_at );
+      $tweet.appendTo(".newsfeed");
+      index -= 1
+    }
   }
   function douglascalhoun(){
-    var randomize = streams.users.douglascalhoun[Math.floor(Math.random() * streams.users.douglascalhoun.length)]; 
-    var $tweet = $('<div class= userTweet></div>');
-    $tweet.text('@' + randomize.user + ': ' + randomize.message + ' posted at ' + randomize.created_at );
-    $tweet.appendTo(".newsfeed");
+    generateRandomTweet()
+    var $main = $('.main');
+    $main.html('');
+    var index = streams.users.douglascalhoun.length-1;
+    while ( index >= 0){
+      var randomize = streams.users.douglascalhoun[Math.floor(Math.random() * streams.users.douglascalhoun.length)]; 
+      var $tweet = $('<div class= userTweet></div>');
+      $tweet.text('@' + randomize.user + ': ' + randomize.message + ' posted at ' + randomize.created_at );
+      $tweet.appendTo(".newsfeed");
+      index -=1
+    }
   }
   function shawndrost(){
-    var randomize = streams.users.shawndrost[Math.floor(Math.random() * streams.users.shawndrost.length)]; 
-    var $tweet = $('<div class= userTweet></div>');
-    $tweet.text('@' + randomize.user + ': ' + randomize.message + ' posted at ' + randomize.created_at );
-    $tweet.appendTo(".newsfeed");
+    generateRandomTweet()
+    var $main = $('.main');
+    $main.html('');
+    var index = streams.users.mracus.length-1;
+    while ( index >= 0){
+      var randomize = streams.users.shawndrost[Math.floor(Math.random() * streams.users.shawndrost.length)]; 
+      var $tweet = $('<div class= userTweet></div>');
+      $tweet.text('@' + randomize.user + ': ' + randomize.message + ' posted at ' + randomize.created_at );
+      $tweet.appendTo(".newsfeed");  
+      index -=1
+    }
   }
+
+ 
